423d67ff5f16d62d6ed03348dff8f1b3940e3f1d
krishnanandamca <94525868+krishnanandamca@users.noreply.github.com>
2022-03-31 15:37:49 +0530

    Add files via upload

---
+++ Areaofcircle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areaofcircle;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sjcet
+ */
+public class Areaofcircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the radius");
+        double r=s.nextDouble();
+        double area=(3.14*r*r);
+        System.out.println("area of circle is:",+Areaofcircle);
+        
+    }
+    
+}
