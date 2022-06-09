fc5aaaca53a398c5de739eaa089b5e3db7b2c4f2
krishnanandamca <94525868+krishnanandamca@users.noreply.github.com>
2022-06-09 16:13:47 +0530

    Add files via upload

---
+++ Mouseevents.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mouseevents;
+
+/**
+ *
+ * @author sjcet
+ */
+import java.awt.*;  
+import java.awt.event.*;  
+
+public class Mouseevents extends Frame implements MouseListener {
+
+      Label l;  
+    Mouseevents(){  
+        addMouseListener(this);  
+          
+        l=new Label();  
+        l.setBounds(20,50,100,20);  
+        add(l);  
+        setSize(300,300);  
+        setLayout(null);  
+        setVisible(true);  
+    }  
+    public void mouseClicked(MouseEvent e) {  
+        l.setText("Mouse Clicked");  
+    }  
+    public void mouseEntered(MouseEvent e) {  
+        l.setText("Mouse Entered");  
+    }  
+    public void mouseExited(MouseEvent e) {  
+        l.setText("Mouse Exited");  
+    }  
+    public void mousePressed(MouseEvent e) {  
+        l.setText("Mouse Pressed");  
+    }  
+    public void mouseReleased(MouseEvent e) {  
+        l.setText("Mouse Released voi");  
+    } 
+    
+    public static void main(String[] args) {
+       new  Mouseevents();  
+    }
+    
+}
+
+
